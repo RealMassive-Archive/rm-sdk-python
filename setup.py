d54064811c215ee28a5bb6d708dbc5b6c7c25b59
wtrdrnkr <michael.musslewhite@realmassive.com>
2016-05-31 11:16:20 -0500

    Adding packages in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from distutils.core import setup
 setup(
     name="realmassive_sdk_python",
     version="0.1",
-    description="RealMassive's Python SDK"
+    description="RealMassive's Python SDK",
+    packages=["realmassive"],
 )
