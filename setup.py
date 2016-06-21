04e4da72f227d41489cdb7f1ff43e3e3783b51d1
Jairo Vasquez <jairo.vasquez@gmail.com>
2016-06-21 11:09:35 -0500

    Change setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="realmassive_sdk_python",
-    version="0.1",
+    version="0.2.0",
     description="RealMassive's Python SDK",
     packages=["realmassive_sdk"],
 )
